9e681945a574cf0ede9aa99ec1206cf162447c3e
Zhu Liang <ymblender@gmail.com>
2026-01-22 19:18:17 -0600

    fix: support blocklet bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createBlockletPlugin } from 'vite-plugin-blocklet';
 import path from 'path';
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), createBlockletPlugin()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
         '@lib': path.resolve(__dirname, 'lib'),
       },
-    },
-    build: {
-      outDir: '.blocklet/client',
-      emptyOutDir: true,
     },
     server: {
       port: 3000,
